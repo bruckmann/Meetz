f5e16eb8e6cc25c05315bc30c461d843901bbc8d
Jardel Urban <jardelurban3@gmail.com>
2021-11-10 03:19:28 -0300

    First commit loginPage

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:meetz/home/home_page.dart';
+import 'package:meetz/login/login_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Meetz", home: HomePage()); //pagina que você vai trabalhar
+        title: "Meetz", home: LoginPage()); //pagina que você vai trabalhar
   }
 }
