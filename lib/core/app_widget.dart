c2d01a076362a7e42f19c7212f3998ced1c59076
Joaquim Lagos <joaquim.lagos2000@gmail.com>
2021-11-17 19:21:21 -0300

    create-splash

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 import 'package:meetz/pages/signin/signin_page.dart';
 import 'package:meetz/pages/signup/signup_page.dart';
+=======
+import 'package:meetz/core/splash_page.dart';
+//import 'package:meetz/core/s.dart';
+>>>>>>> create-splash
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+<<<<<<< HEAD
         title: "Meetz", home: SignUpPage()); //pagina que você vai trabalhar
+=======
+        title: "Meetz", home: SplashPage()); //pagina que você vai trabalhar
+>>>>>>> create-splash
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meetz/core/app_widget.dart';
+//import 'package:meetz/core/splash_page.dart';
 
 import 'core/app_widget.dart';
 void main() {
